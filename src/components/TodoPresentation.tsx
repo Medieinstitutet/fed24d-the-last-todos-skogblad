908f7c020e50bc85cb6b82cebd88faf835c3f73b
Alicia <a.skogblad@gmail.com>
2025-06-02 15:28:57 +0200

    🎨 Replace "done"-emoji with checkbox component

--- src/components/TodoPresentation.tsx
+++ src/components/TodoPresentation.tsx
@@ -8,7 +8,7 @@ export const TodoPresentation = ({ todo }: TodoPresentationProps) => {
   return (
     <ul>
       <li>
-        {todo.name} | Done: {todo.done ? "👍" : "👎"}
+        {todo.name} | Done: <input type="checkbox" checked={todo.done} />
       </li>
     </ul>
   )
