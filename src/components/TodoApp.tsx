300a07a34b575b23743ffdcddd91c7e01a409a50
Alicia <a.skogblad@gmail.com>
2025-06-03 15:52:58 +0200

    :sparkles: Add toggle function to mark todo as done. & add + 1 etc for unique ID

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -4,17 +4,31 @@ import { Todos } from "./Todos";
 
 export const TodoApp = () => {
   const [todos, setTodos] = useState<Todo[]>([
-    { id: Date.now(), name: "Prepare dinner", done: false },
-    { id: Date.now(), name: "Clean the bathroom", done: false },
-    { id: Date.now(), name: "Take a bath", done: false },
-    { id: Date.now(), name: "Play with the dog", done: true },
-    { id: Date.now(), name: "Watch Netflix", done: false },
+    { id: Date.now() + 1, name: "Prepare dinner", done: false },
+    { id: Date.now() + 2, name: "Clean the bathroom", done: false },
+    { id: Date.now() + 3, name: "Take a bath", done: false },
+    { id: Date.now() + 4, name: "Play with the dog", done: false },
+    { id: Date.now() + 5, name: "Watch Netflix", done: false },
   ]);
-  
+
+  // Function to click the checkbox = todo done = todo removed from UI
+  const handleToggleTodo = (id: number) => {
+    setTodos(currentTodos => {
+      const updatedTodoList = currentTodos.map(todo => todo.id === id ? {...todo, done: true } : todo);
+    
+      console.log("All todos:", updatedTodoList);
+
+      return updatedTodoList.filter(todo => !todo.done)
+    
+    });
+  }
+
+  console.log("Current Todos:", todos)
+
   return (
     <>
       <h1>My Todo List</h1>
-      < Todos todos={todos} />
+      < Todos todos={todos} onToggle={handleToggleTodo} />
     </>
   );
 }
