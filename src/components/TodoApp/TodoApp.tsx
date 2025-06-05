92e36660a490cd8829bd8f0c989efeee27cf8fa6
Alicia <a.skogblad@gmail.com>
2025-06-05 18:59:10 +0200

    :sparkles: :recycle: add sort function and moved "firstTodos" to make it(id) work smoothly

--- src/components/TodoApp/TodoApp.tsx
+++ src/components/TodoApp/TodoApp.tsx
@@ -2,23 +2,24 @@ import { useState } from "react";
 import type { Todo } from "../../models/Todo";
 import { Todos } from "../Todos/Todos";
 import { AddTodo } from "../AddTodo/AddTodo";
-import style from "./TodoApp.module.scss"
+import style from "./TodoApp.module.scss";
+
+const createId = () => Date.now() + Math.floor(Math.random() * 1000);
+
+const firstTodos: Todo[] = [
+  { id: createId(), name: "Prepare dinner", done: false },
+  { id: createId(), name: "Clean the bathroom", done: false },
+  { id: createId(), name: "Take a bath", done: false },
+  { id: createId(), name: "Play with the dog", done: false },
+  { id: createId(), name: "Watch Netflix", done: false },
+]
 
 export const TodoApp = () => {
-
-  const firstTodos: Todo[] = [
-    { id: Date.now() + 1, name: "Prepare dinner", done: false },
-    { id: Date.now() + 2, name: "Clean the bathroom", done: false },
-    { id: Date.now() + 3, name: "Take a bath", done: false },
-    { id: Date.now() + 4, name: "Play with the dog", done: false },
-    { id: Date.now() + 5, name: "Watch Netflix", done: false },
-  ]
-
   const [todos, setTodos] = useState<Todo[]>(
-    JSON.parse(localStorage.getItem("todos") || JSON.stringify(firstTodos))
+    () => JSON.parse(localStorage.getItem("todos") || JSON.stringify(firstTodos))
   );
 
-  // Function to click the checkbox = todo done = todo removed from UI
+  // Function to click the checkbox = todo done
   const handleToggleTodo = (id: number) => {
     setTodos(currentTodos => {
      return currentTodos.map(todo => todo.id === id ? {...todo, done: !todo.done } : todo );
@@ -37,9 +38,16 @@ export const TodoApp = () => {
     setTodos([...todos, t]);
   }
 
+  // Sort function
+  const sortByAlpha = () => {
+    const sortedTodos = [...todos].sort((todo1, todo2) => todo1.name.localeCompare(todo2.name));
+    setTodos(sortedTodos);
+  }
+
   return (
     <>
       <h1>My Todo List</h1>
+      <button onClick={sortByAlpha} className={style.sortButton}>Sort by name</button>
 
       <section className={style.activeTodos}>
         <h2>Active todos:</h2>
